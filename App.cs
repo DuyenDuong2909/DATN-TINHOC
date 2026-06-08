ebb65cb7826d604b864b314ddd9883d3cfd2d151
DuyenDuong2909 <duyenduong4299@gmail.com>
2026-06-08 08:16:12 +0700

    first commit

---
+++ App.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace AutoCADToRevitApplication
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "Dựng mô hình Revit";
+            application.CreateRibbonTab(tabName);
+
+            RibbonPanel panel = application.CreateRibbonPanel(tabName, "Đồ án tốt nghiệp");
+
+            string assemblyPath = typeof(App).Assembly.Location;
+
+            PushButtonData buttonData = new PushButtonData(
+                name: "OpenConverter",
+                text: "CAD 2D\nRevit 3D",
+                assemblyName: assemblyPath,
+                className: "AutoCADToRevitApplication.Command"
+            );
+
+            PushButton button = panel.AddItem(buttonData) as PushButton;
+            string iconPath = Path.Combine(
+                    Path.GetDirectoryName(assemblyPath),
+                    "Images", "cad2revit.png");
+
+            if (File.Exists(iconPath))
+            {
+                button.LargeImage = new BitmapImage(
+                    new Uri(iconPath, UriKind.Absolute));
+            }
+            button.ToolTip = "Mở công cụ chuyển đổi file AutoCAD sang Revit 3D";
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Command.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCADToRevitApplication
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData,
+                              ref string message,
+                              ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+
+            var window = new Views.MainWindow(uiApp);
+            window.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}
